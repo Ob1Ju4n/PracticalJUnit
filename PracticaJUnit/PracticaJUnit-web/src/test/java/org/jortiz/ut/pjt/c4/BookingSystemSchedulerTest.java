3dbcdbc0d462a8468038acf1bfd6db47b6de9b6d
Juan Pablo Ortiz <juan1087@gmail.com>
2014-08-03 22:47:24 -0500

    Excercise 4.10.3 skeleton.

---
+++ PracticaJUnit/PracticaJUnit-web/src/test/java/org/jortiz/ut/pjt/c4/BookingSystemSchedulerTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jortiz.ut.pjt.c4;
+
+/**
+ *
+ * @author juan.ortiz
+ */
+public class BookingSystemSchedulerTest {
+
+    public void shouldCreateResourceToBookTo(){}
+    
+    public void shouldCheckHourIsRegularHour(){}
+    
+    public void shouldCheckHourArgumentIsValid(){}
+            
+    public void shouldBookAGivenHour(){}
+    
+    public void shouldReturnAListOfBookedHours(){}
+    
+    public void shoudBookEveryBookableHour(){}
+    
+    public void shouldRestrictDoubleBookingForAGivenHour(){}
+    
+}
